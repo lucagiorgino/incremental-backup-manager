7ee00effe8808baac616b725d57ec038857773bc
AndreaBruno97 <andbruno97@gmail.com>
2020-08-05 12:27:56 +0200

    fix: minor changes in testOpenSSL

--- testOpenSSL/main.cpp
+++ testOpenSSL/main.cpp
@@ -49,10 +49,11 @@ class Hash {
 private:
     unsigned char md_value[EVP_MAX_MD_SIZE];
     int md_len;
+    Hash();
 public:
-    bool computeHash(const std::string filename) {
+    Hash(const std::string filename){
         std::ifstream myfile (filename);
-        char* buf = new char [BUF_SIZE];
+        char buf[BUF_SIZE];
 
         if(!myfile.is_open()) {
             std::cout << "Couldn't open input file, try again" << std::endl;
@@ -60,62 +61,78 @@ public:
         }
 
         EVP_MD_CTX *md_ctx;
-        md_ctx = EVP_MD_CTX_new();
-        EVP_MD_CTX_init(md_ctx);
 
-        EVP_DigestInit(md_ctx, EVP_sha256());
+        if ( (md_ctx = EVP_MD_CTX_new() ) == NULL) {
+            // hadleErrors();
+            abort();
+        }
 
-        std::cout<<"start reading..."<<std::endl;
+        if( EVP_MD_CTX_init(md_ctx) != 1){
+            // handleErrors();
+            abort();
+        }
+
+
+        if( EVP_DigestInit(md_ctx, EVP_sha256()) != 1){
+            // handleErrors();
+            abort();
+        }
+
+        // std::cout<<"start reading..."<<std::endl;
         while ( !myfile.eof() ) {
-            myfile.read (buf,BUF_SIZE);
+            myfile.read (buf,BUF_SIZE); // può dare errori
             int n = myfile.gcount();
-            EVP_DigestUpdate(md_ctx, buf,n);
+            if ( EVP_DigestUpdate(md_ctx, buf,n) != 1)
+                abort();
         }
 
 
         // EVP_DigestFinal_ex(md_ctx, md_value, (&md_len))
         if(EVP_DigestFinal_ex(md_ctx, this->md_value, reinterpret_cast<unsigned int *> (&this->md_len)) != 1) {
             std::cout << "Digest computation problem\n";
-            exit(1);
+            abort();
         }
-
-        std::cout << "The digest is: "<< std::endl;
-        for(int i = 0; i < md_len; i++)
-            std::cout<< std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(md_value[i]);
-        std::cout << std::endl;
 
         EVP_MD_CTX_free(md_ctx);
         myfile.close();
-        delete[] buf;
-
-        return  true;
-    };
+    }
 
 
     inline bool operator==( const Hash& input){
         if(this->md_len != input.md_len) return false;
 
         if( CRYPTO_memcmp(this->md_value, input.md_value, this->md_len) != 0 ) {
-            printf("The digests are different!\n");
+            // printf("The digests are different!\n");
             return false;
         }
         else {
-            printf("Same digest. Content identical.\n");
+            // printf("Same digest. Content identical.\n");
             return true;
         }
     }
 
+    void printHash(){
+        std::cout << "The digest is: ";
+        for(int i = 0; i < md_len; i++)
+            std::cout<< std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(md_value[i]);
+        std::cout << std::endl;
+    }
 };
-
 int main(){
 
-    Hash file1;
-    file1.computeHash("../example.txt");
+    Hash hashfile1("example.txt");
+    Hash hashfile2("example.txt");
 
-    Hash file2;
-    file2.computeHash("../example2.txt");
+    hashfile1.printHash();
+    hashfile2.printHash();
 
-    if(file1 == file2);
+
+
+
+    if( Hash("example.txt") == Hash("example2.txt") )
+        printf("Same digest. Content identical.\n");
+    else
+        printf("The digests are different!\n");
 
     return 0;
 }
