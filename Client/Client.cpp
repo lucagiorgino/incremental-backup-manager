76741fac59c564bdfa6712a592c65f4e30285266
cosimochetta <minochetta@gmail.com>
2020-08-26 12:30:09 +0200

    feat: implemented client quit

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -3,11 +3,11 @@
 
 Client::Client(std::string name) :
         resolver_(io_context_),
-        socket_(io_context_), fileWatcher( std::chrono::duration<int, std::milli>(DELAY),
-                                           [this](const std::string &path, FileStatus fileStatus) {
-                                               Action action{path, fileStatus};
-                                               this->actions.push(action);
-                                           }) {
+        socket_(io_context_), fileWatcher(std::chrono::duration<int, std::milli>(DELAY),
+                                          [this](const std::string &path, FileStatus fileStatus) {
+                                              Action action{path, fileStatus};
+                                              this->actions.push(action);
+                                          }) {
 
     try {
         std::string path_string;
@@ -16,12 +16,12 @@ Client::Client(std::string name) :
         int is_signedup = 0;
 
         const std::filesystem::path backup_path = "../path";
-        if(!std::filesystem::exists(backup_path)){
+        if (!std::filesystem::exists(backup_path)) {
             // new path
-            std::cout<<"insert existing path to create accout: ";
+            std::cout << "insert existing path to create accout: ";
             std::cin >> path_string; // un path già esistente
-            while (!std::filesystem ::exists(path_string)) {
-                std::cout<<"path not found, try again: ";
+            while (!std::filesystem::exists(path_string)) {
+                std::cout << "path not found, try again: ";
                 std::cin >> path_string; // un path già esistente
             }
 
@@ -46,17 +46,17 @@ Client::Client(std::string name) :
         boost::asio::streambuf request_output;
         std::istream input_stream(&request_input);
         std::ostream output_stream(&request_output);
-        output_stream << std::setw(sizeof(int)) <<  std::setfill('0') << name.length() << "\n"
-                       << name << "\n";
+        output_stream << std::setw(sizeof(int)) << std::setfill('0') << name.length() << "\n"
+                      << name << "\n";
         boost::asio::write(socket_, request_output);
 
         boost::asio::read(socket_, request_input, boost::asio::transfer_exactly(2));
         input_stream >> is_signedup;
-        std::cout << "is_signedup: " << is_signedup <<std::endl;
+        std::cout << "is_signedup: " << is_signedup << std::endl;
 
-        if(!is_signedup){
+        if (!is_signedup) {
             std::string password1;
-            do{
+            do {
                 std::cout << "You are not signed up. \n Insert new password: ";
                 std::cin >> password;
 
@@ -68,96 +68,56 @@ Client::Client(std::string name) :
 
             output_stream << password.length() << "\n" << password << "\n";
             boost::asio::write(socket_, request_output);
-            std::cout << "Ok, You are now signed up!"<<std::endl;
+            std::cout << "Ok, You are now signed up!" << std::endl;
         }
         std::cout << "LOG IN" << std::endl;
-        while(is_authenticated == 0){
+        while (is_authenticated == 0) {
             std::cout << "Insert password: ";
             std::cin >> password;
             output_stream << password.length() << "\n" << password << "\n";
             boost::asio::write(socket_, request_output);
             boost::asio::read(socket_, request_input, boost::asio::transfer_exactly(2));
             input_stream >> is_authenticated;
-            std::cout << "is_authenticated: " << is_authenticated <<std::endl;
+            std::cout << "is_authenticated: " << is_authenticated << std::endl;
         }
 
 
         fileWatcherThread = std::thread([this]() {
             std::unordered_map<std::string, Hash> initial_status;
-            this->fileWatcher.start(this->path.string(),initial_status);
+            this->fileWatcher.start(this->path.string(), initial_status);
         });
 
         actionsConsumer = std::thread([this]() {
-            while (true) {
-                std::optional<Action> action = actions.pop();
+            std::optional<Action> action;
+
+            do{
+                action = actions.pop();
                 if (action.has_value()) {
                     send_action(action.value());
                 }
-            }
+            } while(!action.has_value());
+
+            boost::asio::streambuf request;
+            std::ostream request_stream(&request);
+
+            std::string path = "0000";
+            request_stream << ActionType::quit << "\n"
+                           << std::setw(sizeof(int)) << std::setfill('0') << path.length() << "\n"
+                           << path << "\n";
+
+            boost::asio::write(socket_, request);
         });
-
-
-
-        /*
-        Action action;
-        action.fileStatus = FileStatus::created;
-        action.path = "../dirA";
-        send_action(action);
-
-        action.fileStatus = FileStatus::created;
-        action.path = "../dirA/loremipsum";
-
-        send_action(action);
-        */
-        /*std::string data;
-        boost::system::error_code error;
-        name += '\0';
-        password += '\0';
-        std::cout << name << name.length() << " " << password << password.length() << std::endl;
-
-        size_t len;
-        std::cout << "writing name" << std::endl;
-        //** TO-DO: Criptare la connessione per lo scambio di username/password
-        boost::asio::write(socket_, boost::asio::buffer(name, name.length()), error);
-
-        std::cout << "writing password" << std::endl;
-        boost::asio::write(socket_, boost::asio::buffer(password, password.length()), error);
-        data.clear();
-
-        std::cout << "reading name" << std::endl;
-        len = boost::asio::read_until(socket_, boost::asio::dynamic_buffer(data), "\0", error);
-        std::string namercv;
-        std::getline(std::istringstream(data), namercv, '\0');
-        if (error && error != boost::asio::error::eof)
-            throw boost::system::system_error(error); // Some other error.
-
-        std::cout << "data " << data << " - " << data.length()  << std::endl;
-        std::cout << namercv << " - " << len  << std::endl;
-
-        std::cout << "reading password" << std::endl;
-        len = boost::asio::read_until(socket_, boost::asio::dynamic_buffer(data), "\0",error);
-        if (error && error != boost::asio::error::eof)
-            throw boost::system::system_error(error);
-        std::string passrecv;
-        std::getline(std::istringstream(data), passrecv, '\0');
-
-        std::cout << "data " << data << " - " << data.length()  << std::endl;
-        std::cout << passrecv << " - " << len << std::endl;
-         */
 
     } catch (std::exception &exception) {
         std::cerr << exception.what() << std::endl;
         throw exception;
     }
-    /*
-    std::thread fwThread([this]() {
-        fileWatcher.start();
-    });*/
-
 }
 
-Client::~Client(){
+Client::~Client() {
+    fileWatcher.stop();
     fileWatcherThread.join();
+
     actions.terminate();
     actionsConsumer.join();
 }
@@ -183,7 +143,7 @@ void Client::send_action(Action action) {
             break;
     }
 
-    if(actionType == ActionType::ignore){
+    if (actionType == ActionType::ignore) {
         return;
     }
 
@@ -202,22 +162,19 @@ void Client::send_action(Action action) {
 
 
     std::string cleaned_path = action.path.string();
-    size_t pos = cleaned_path.find(path.string() );
-    cleaned_path.erase( pos, path.string().length() );
-    std::cout << "CLEANED PATH: " << action.path  << std::endl;
+    size_t pos = cleaned_path.find(path.string());
+    cleaned_path.erase(pos, path.string().length());
+    std::cout << "CLEANED PATH: " << action.path << std::endl;
 
 
-
-    request_stream  << actionType << "\n"
-     //              << std::setw(sizeof(int)) <<  std::setfill('0') << action.path.string().length() << "\n"
-     //              << action.path.string() << "\n";
-                    << std::setw(sizeof(int)) <<  std::setfill('0') << cleaned_path.length() << "\n"
-                    << cleaned_path << "\n";
+    request_stream << actionType << "\n"
+                   << std::setw(sizeof(int)) << std::setfill('0') << cleaned_path.length() << "\n"
+                   << cleaned_path << "\n";
 
     size_t len = boost::asio::write(socket_, request);
 
     std::cout << isDirectory << "SIZEOF actiontype" << actionType << "-" << action.path.string().length() << std::endl;
-    std::cout<< "SENT " << len << " BYTES" << std::endl;
+    std::cout << "SENT " << len << " BYTES" << std::endl;
 
     //boost::asio::write(socket_, boost::asio::buffer(actionType, sizeof(int)));
 
--- Client/main.cpp
+++ Client/main.cpp
@@ -2,7 +2,15 @@
 #include "Client.h"
 int main() {
     Client client1{ "pino"};
+
+    std::string quit;
+    do{
+        std::cout << "Insert \"Q\" to quit: ";
+        std::cin >> quit;
+    }while(quit.compare("Q") != 0);
+
     //Client client2{"../dir2", "aldo", "baglio"};
     //Client client3{"../dir3", "aldo", "baglio"};
+    std::cout << "TERMINATED" << std::endl;
     return 0;
 }
