2f130e65edf169c4cdb07a5c74bf3d244131836b
AndreaBruno97 <andbruno97@gmail.com>
2020-09-08 15:58:40 +0200

    update: test on metadata test and set

---
+++ provaMetadati/cmake-build-debug/CMakeFiles/provaMetadati.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/provaMetadati.dir/main.cpp.o"
+  "provaMetadati"
+  "provaMetadati.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/provaMetadati.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
